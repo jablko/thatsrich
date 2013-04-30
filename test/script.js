e5b39095107ac57badd648691668a779e753c697
Jack Bates <jack@nottheoilrig.com>
2013-04-30 10:17:37 -0700

    Don't swallow blank line

--- test/script.js
+++ test/script.js
@@ -1,6 +1,7 @@
 test('Table', function () {
     var $textarea = $([
       '<textarea id="wpTextbox1">',
+      'Before',
       '{|',
       '|a',
       '|b',
@@ -14,17 +15,59 @@ test('Table', function () {
       '|h',
       '|i',
       '|}',
+      'After',
       '</textarea>',
       '<script src="../thatsrich.js">'].join('\n')).appendTo(document.body);
 
     equal($textarea.val(), [
+      'Before',
       '   +---+---+---+',
       '   | a | b | c |',
       '   +---+---+---+',
       '   | d | e | f |',
       '   +---+---+---+',
       '   | g | h | i |',
-      '   +---+---+---+'].join('\n') + '\n');
+      '   +---+---+---+',
+      'After',
+      ''].join('\n'));
+
+    $textarea.remove();
+  });
+
+test('Blank line', function () {
+    var $textarea = $([
+      '<textarea id="wpTextbox1">',
+      'Before',
+      '',
+      '{|',
+      '|a',
+      '|b',
+      '|c',
+      '|-',
+      '|d',
+      '|e',
+      '|f',
+      '|-',
+      '|g',
+      '|h',
+      '|i',
+      '|}',
+      'After',
+      '</textarea>',
+      '<script src="../thatsrich.js">'].join('\n')).appendTo(document.body);
+
+    equal($textarea.val(), [
+      'Before',
+      '',
+      '   +---+---+---+',
+      '   | a | b | c |',
+      '   +---+---+---+',
+      '   | d | e | f |',
+      '   +---+---+---+',
+      '   | g | h | i |',
+      '   +---+---+---+',
+      'After',
+      ''].join('\n'));
 
     $textarea.remove();
   });
--- thatsrich.js
+++ thatsrich.js
@@ -2,13 +2,13 @@ var textarea = document.getElementById('wpTextbox1');
 
 var end = 0,
   match,
-  regexp = /^\s*{\|(?:.|\s)*?^\s*\|}/gm,
+  regexp = /^ *{\|(?:.|\n)*?^ *\|}/gm,
   value = '';
 
 // https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/RegExp/exec#Finding_successive_matches
 while ((match = regexp.exec(textarea.value)) !== null) {
-  var cols = [], cells = textarea.value.slice(match.index, regexp.lastIndex - 2).split(/^\s*\|-/gm).map(function (itm) {
-        return itm.split(/^\s*[|!]/gm).slice(1).map(function (itm, idx) {
+  var cols = [], cells = textarea.value.slice(match.index, regexp.lastIndex - 2).split(/^ *\|-/gm).map(function (itm) {
+        return itm.split(/^ *[|!]/gm).slice(1).map(function (itm, idx) {
             itm = itm.trim();
             cols[idx] = Math.max(cols[idx] || 0, itm.length);
 
