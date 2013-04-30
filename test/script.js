2ab0de246e75ef89142fa845623d46a011f4da56
Jack Bates <jack@nottheoilrig.com>
2013-04-30 12:04:19 -0700

    Move cursor to start or end of cell

--- test/script.js
+++ test/script.js
@@ -1,3 +1,5 @@
+var browserBot = new BrowserBot();
+
 test('Table', function () {
     var $textarea = $([
       '<textarea id="wpTextbox1">',
@@ -68,6 +70,72 @@ test('Blank line', function () {
       '   +---+---+---+',
       'After',
       ''].join('\n'));
+
+    $textarea.remove();
+  });
+
+test('Click start', function () {
+    var $textarea = $([
+      '<textarea id="wpTextbox1">',
+      'Before',
+      '{|',
+      '|a',
+      '|b',
+      '|c',
+      '|-',
+      '|d',
+      '|e',
+      '|f',
+      '|-',
+      '|g',
+      '|h',
+      '|i',
+      '|}',
+      'After',
+      '</textarea>',
+      '<script src="../thatsrich.js">'].join('\n')).appendTo(document.body);
+
+    $textarea[0].selectionStart = $textarea[0].selectionEnd = 66;
+
+    browserBot.triggerMouseEvent($textarea[0], 'click', true);
+
+    equal($textarea[0].selectionStart, 67);
+    equal($textarea[0].selectionEnd, 67);
+
+    equal($('div:eq(-1)').html(), '');
+
+    $textarea.remove();
+  });
+
+test('Click end', function () {
+    var $textarea = $([
+      '<textarea id="wpTextbox1">',
+      'Before',
+      '{|',
+      '|a',
+      '|b',
+      '|c',
+      '|-',
+      '|d',
+      '|e',
+      '|f',
+      '|-',
+      '|g',
+      '|h',
+      '|i',
+      '|}',
+      'After',
+      '</textarea>',
+      '<script src="../thatsrich.js">'].join('\n')).appendTo(document.body);
+
+    $textarea[0].selectionStart = $textarea[0].selectionEnd = 69;
+
+    browserBot.triggerMouseEvent($textarea[0], 'click', true);
+
+    equal($textarea[0].selectionStart, 68);
+    equal($textarea[0].selectionEnd, 68);
+
+    equal($('div:eq(-1)').html(), '');
 
     $textarea.remove();
   });
--- thatsrich.js
+++ thatsrich.js
@@ -1,4 +1,12 @@
-var textarea = document.getElementById('wpTextbox1');
+var indent = 3,
+  rowIdx, colIdx,
+  rowStart, colStart;
+
+var textarea = document.getElementById('wpTextbox1'),
+  selectionStart = textarea.selectionStart,
+  selectionEnd = textarea.selectionEnd;
+
+var $message = $('<div>').appendTo(document.body);
 
 var end = 0,
   match,
@@ -7,18 +15,75 @@ var end = 0,
 
 // https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/RegExp/exec#Finding_successive_matches
 while ((match = regexp.exec(textarea.value)) !== null) {
-  var cols = [], cells = textarea.value.slice(match.index, regexp.lastIndex - 2).split(/^ *\|-/gm).map(function (itm) {
-        return itm.split(/^ *[|!]/gm).slice(1).map(function (itm, idx) {
-            itm = itm.trim();
-            cols[idx] = Math.max(cols[idx] || 0, itm.length);
+  var tableStart = match.index,
+    rows = [], cols = [], cells = [],
+    content = [];
 
-            return itm;
-          });
-      }),
-    separator = '   +' + cols.map(function (itm) { return Array(itm + 3).join('-') }).join('+') + '+';
+  textarea.value.slice(tableStart, regexp.lastIndex - 2).split(/^ *\|-/gm).forEach(function (itm, rowIdx) {
+      rows[rowIdx] = 1;
+      cells[rowIdx] = [];
+      content[rowIdx] = [];
 
-  value += textarea.value.slice(end, match.index) + separator + '\n' + cells.map(function (itm) { return '   | ' + itm.map(function (itm, idx) { return itm + Array(cols[idx] - itm.length + 1).join(' ') }).join(' | ') + ' |\n' }).join(separator + '\n') + separator;
+      itm.split(/^ *[|!]/gm).slice(1).forEach(function (itm, colIdx) {
+          content[rowIdx][colIdx] = itm = itm.trim();
+          cells[rowIdx][colIdx] = itm.length;
+          cols[colIdx] = Math.max(cols[colIdx] || 0, itm.length);
+        });
+    });
+
+  var separator = Array(indent + 1).join(' ') + '+' + cols.map(function (itm) { return Array(itm + 3).join('-') }).join('+') + '+';
+
+  value += textarea.value.slice(end, tableStart) + separator + '\n' + content.map(function (itm) { return Array(indent + 1).join(' ') + '| ' + itm.map(function (itm, idx) { return itm + Array(cols[idx] - itm.length + 1).join(' ') }).join(' | ') + ' |\n' }).join(separator + '\n') + separator;
   end = regexp.lastIndex;
 }
 
 textarea.value = value + textarea.value.slice(end);
+
+$(textarea).on('click', function (evt) {
+    var rowLength = indent + cols.reduce(function (a, b) { return a + b }) + cols.length * 3 + 2,
+      rowOffset = (this.selectionStart - tableStart) % rowLength - indent;
+
+    var start = tableStart + indent + rowLength;
+    for (var idx = 0; idx < rows.length; idx += 1) {
+      if (this.selectionStart > start && this.selectionStart < start + rows[idx] * rowLength - indent - 1) {
+        var nstRowIdx = idx,
+          nstRowStart = start;
+
+        break;
+      } else {
+        start += (rows[idx] + 1) * rowLength;
+      }
+    }
+
+    var start = 0;
+    for (var idx = 0; idx < cols.length; idx += 1) {
+      if (rowOffset > start && rowOffset < start + cols[idx] + 4) {
+        var nstColIdx = idx,
+          nstColStart = start;
+
+        break;
+      } else {
+        start += cols[idx] + 3;
+      }
+    }
+
+    if (nstRowIdx !== undefined && nstColIdx !== undefined) {
+      rowIdx = nstRowIdx; colIdx = nstColIdx;
+      rowStart = nstRowStart; colStart = nstColStart;
+
+      if (rowOffset < colStart + 2) {
+        selectionStart = selectionEnd = this.selectionStart = this.selectionEnd = rowStart + colStart + 2;
+      } else if (rowOffset > colStart + 2 + cells[rowIdx][colIdx]) {
+        selectionStart = selectionEnd = this.selectionStart = this.selectionEnd = rowStart + colStart + 2 + cells[rowIdx][colIdx];
+      } else {
+        selectionStart = selectionEnd = this.selectionStart;
+      }
+
+      $message.html('');
+    } else {
+      this.selectionStart = selectionStart;
+      this.selectionEnd = selectionEnd;
+
+      $message.html('Click in a cell to edit.');
+    }
+  });
